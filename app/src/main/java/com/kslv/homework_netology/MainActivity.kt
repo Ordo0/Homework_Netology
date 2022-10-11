8b39a290d39f22cbbb23ae11364ade2c60b842fd
Vyacheslav Kolabukhov <80225471+Ordo0@users.noreply.github.com>
2022-10-11 15:20:50 +0300

    HW_2

--- app/src/main/java/com/kslv/homework_netology/MainActivity.kt
+++ app/src/main/java/com/kslv/homework_netology/MainActivity.kt
@@ -2,23 +2,82 @@ package com.kslv.homework_netology
 
 import android.os.Bundle
 import android.util.Log
-import android.widget.TextView
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.ListView
+import android.widget.ProgressBar
+import android.widget.SimpleAdapter
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     val TAG: String = "MainActivity"
-    private val name = "Ivan"
-    private val surname = "Ivanov"
-    private val age = 37
-    private val height = 172.2
 
+    lateinit var requestInput: TextInputEditText
+
+    lateinit var podsAdapter: SimpleAdapter
+
+    val pods = mutableListOf<HashMap<String, String>>(
+        HashMap<String, String>().apply {
+            put("Title", "Title 1")
+            put("Content", "Content 1")
+        },
+        HashMap<String, String>().apply {
+            put("Title", "Title 2")
+            put("Content", "Content 2")
+        },
+        HashMap<String, String>().apply {
+            put("Title", "Title 3")
+            put("Content", "Content 3")
+        }
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.d(TAG, "start of onCreate function")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val output: TextView = findViewById(R.id.output)
-        "$name $surname age: $age height: $height".also { output.text = it }
-        Log.d(TAG, "end of onCreate function")
+        initViews()
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_stop -> {
+                Log.d(TAG, "click_stop")
+                return true
+            }
+            R.id.action_clear -> {
+                Log.d(TAG, "click_clear")
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun initViews() {
+        val toolbar: MaterialToolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        requestInput = findViewById(R.id.text_input_edit)
+
+        val podList: ListView = findViewById(R.id.pods_list)
+        podsAdapter = SimpleAdapter(
+            applicationContext,
+            pods,
+            R.layout.item_pod,
+            arrayOf("Title", "Content"),
+            intArrayOf(R.id.title, R.id.content)
+        )
+        podList.adapter = podsAdapter
+
+        val btnVoice: FloatingActionButton = findViewById(R.id.voice_input_button)
+        btnVoice.setOnClickListener { Log.d(TAG, "voice") }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
